9e965eaa33b70dac9bce9a38e1bd39c5c2bdec79
duongnhathao <duongnhathao001@gmail.com>
2022-03-05 00:16:22 +0700

    Add Helper

--- src/helpers.php
+++ src/helpers.php
@@ -2,9 +2,6 @@
 
 defined('BASEPATH') or exit('No direct script access allowed');
 
-use app\services\messages\Message;
-use app\services\messages\PopupMessage;
-
 function app_admin_head()
 {
     hooks()->do_action('app_admin_head');
